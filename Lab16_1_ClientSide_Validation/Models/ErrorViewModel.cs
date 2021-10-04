a301b60b46c94bce31a12cf35ccb6bc3ec9326d3
Ryan Coughran <ryancoughran@quickenloans.com>
2021-10-04 15:12:56 -0700

    initial commit

---
+++ Lab16_1_ClientSide_Validation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab16_1_ClientSide_Validation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Lab16_1_ClientSide_Validation/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16_1_ClientSide_Validation.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPass { get; set; }
+    }
+}
